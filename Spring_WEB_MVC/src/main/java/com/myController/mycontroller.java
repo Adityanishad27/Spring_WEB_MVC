f39de77511636ece42d383c4642f91dacde97fcd
Aditya Nishad <adityanishad0001@gmail.com>
2024-07-13 23:12:14 +0530

    Add files via upload

---
+++ Spring_WEB_MVC/src/main/java/com/myController/mycontroller.java
@@ -0,0 +1,40 @@
+package com.myController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class mycontroller {
+	
+	@GetMapping("/LoginPage")
+	
+//@RequestMapping(value="/LoginPage")
+
+	public ModelAndView loginPage() {
+		
+		
+		ModelAndView mav= new ModelAndView();
+		
+		mav.setViewName("OpenLogin");
+		return mav;
+		
+		
+	}
+	
+	
+	@GetMapping("about-us")
+	public ModelAndView aboutpage() {
+		
+		ModelAndView mv=  new  ModelAndView();
+		mv.setViewName("aboutUS");
+		
+		return mv;
+		
+	}
+
+}
---
+++ Spring_WEB_MVC/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2>THIS IS INDEX HOME PAGE </h2>
+
+<a href="LoginPage">LOGIN</a>
+<a href="about-us"> AboutUS</a>
+
+
+
+
+</body>
+</html>
